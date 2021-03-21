196f2f80682639232fe80cfd7588b674022a9165
Ewhan Han <ewhan.han@gmail.com>
2021-03-21 10:52:49 -0400

    useEffect and loading data from Firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,6 +6,34 @@ import IngredientList from './IngredientList';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  //useEffect to do side-effects
+  // side-effects is some logic that does not finish in this render cycle or affects something outside of this jsx scope
+  //useEffect is called after every render cycle
+  useEffect(() => {
+    fetch(
+      'https://cors-anywhere.herokuapp.com/https://learn-react-hooks-6cb7d-default-rtdb.firebaseio.com/ingredients.json'
+    )
+      .then((response) => {
+        //get response.body via response.json() --> returns a promise
+        return response.json();
+      })
+      .then((responseData) => {
+        console.log('GET RESPONSE FROM FIREBASE', responseData);
+        if (responseData) {
+          const loadedIngredients = [];
+          for (const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              ...responseData[key],
+            });
+          }
+          setUserIngredients(loadedIngredients);
+        } else {
+          alert('INGREDIENT DB IS EMPTY!');
+        }
+      });
+  }, []);
 
   //We're going to pass this function to a component via prop
   const addIngredientHandler = (ingredient) => {
